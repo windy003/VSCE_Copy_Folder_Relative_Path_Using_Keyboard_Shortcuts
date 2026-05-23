aded48008ed8ec216c9f71ccdbe1796aba6ab065
windy003 <928920471@qq.com>
2026-05-23 16:02:30 +0800

    调整

--- extension.js
+++ extension.js
@@ -4,24 +4,15 @@ const path = require('path');
 function activate(context) {
   const disposable = vscode.commands.registerCommand(
     'copyFolderRelativePath.copy',
-    async (uri, uris) => {
-      const targets = (uris && uris.length ? uris : uri ? [uri] : []);
-
-      if (!targets.length) {
+    async (uri) => {
+      if (!uri) {
         return;
       }
 
-      const relPaths = targets.map((u) => {
-        const folder = vscode.workspace.getWorkspaceFolder(u);
-        if (folder) {
-          const rel = path.relative(folder.uri.fsPath, u.fsPath);
-          return rel.split(path.sep).join('/');
-        }
-        return vscode.workspace.asRelativePath(u, false).split(path.sep).join('/');
-      });
+      const folder = vscode.workspace.getWorkspaceFolder(uri);
+      const relPath = path.relative(folder.uri.fsPath, uri.fsPath).split(path.sep).join('/');
 
-      const text = relPaths.join('\n');
-      await vscode.env.clipboard.writeText(text);
+      await vscode.env.clipboard.writeText(relPath);
     }
   );
 
